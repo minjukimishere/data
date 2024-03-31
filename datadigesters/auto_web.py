ddcbf9c95711860ddd660359240a0c7bcfca2086
MJ <112530099+minjukimishere@users.noreply.github.com>
2024-03-31 23:43:33 +0900

    Add files via upload
    첫번째 자동화 코드

---
+++ datadigesters/auto_web.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from time import sleep
+
+driver=webdriver.Chrome() #셀레니움 업데이트로 괄호 비워두기
+sleep(3)
+driver.get('https://scholar.google.co.kr/schhp?hl=ko')
+
+
+#라이브러리
+
+#변수 선언부
+
+#입력부
+
+#출력부
+
+
+#driver.quit()
+
